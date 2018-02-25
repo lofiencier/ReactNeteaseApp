21530f56d04bf9120584c65a2ff8693fe6bdb774
[E.N.V.Y] <842028554@qq.com>
2018-02-25 22:58:58 +0800

    theme

---
+++ src/theme.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "@primary-color": "#e65454",
+  "@border-radius-base": "2px"
+};
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,68 +1,220 @@
-var webpack = require("webpack");
-var path = require("path");
-var axios = require("axios");
-// var ExtractTextPlugin =require ('extract-text-webpack-plugin');
+"use strict";
 
-module.exports = {
-  entry: path.join(__dirname, "app/index.jsx"), //很直白，渲染层的唯一入口,无需require 'path'即可使用__dirname?存疑
-  output: {
-    path: path.join(__dirname, "../public"), //出口
-    filename: "bundle.js" //可以添加hash值，id,name,使用不同名字的bundle时，可以使用html-webpack-plugin这个webpack插件生成生成一个自动引用你打包后的JS文件的新index.html,放在此webpack.config.js后面的plugins中
-  },
-  devtool: "eval-source-map", //生成source-map，方便调试，具体有什么用？
-  devServer: {
-    contentBase: "./public", //本地服务器所加载的页面所在的目录
-    historyApiFallback: true, //不跳转
-    inline: true,
-    proxy: {
-      "*": {
-        target: "http://localhost:3000",
-        changeOrigin: true
+module.exports = function(env) {
+  // 生成环境下webpack使用-p参数开启代码压缩
+  // webpack[-dev-server]使用--env dev参数指定编译环境
+  var isDev = env == "dev";
+  var path = require("path");
+  var webpack = require("webpack");
+  var CleanWebpackPlugin = require("clean-webpack-plugin");
+  var HtmlWebpackPlugin = require("html-webpack-plugin");
+  var WebkitPrefixer = require("autoprefixer");
+  var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
+
+  var sourcedir = path.resolve(__dirname, "app/"); // 源码和资源文件的放置位置
+  var outputdir = path.resolve(__dirname, "../public"); // 编译结果的放置位置
+  var theme = require(path.resolve(__dirname, "./theme.js"));
+  console.log("theme:", theme);
+  var webContextRoot = "/"; // 应用的实际访问路径，默认是'/'
+  var hasValue = function(item) {
+    return item != null;
+  };
+  var axios = require("axios");
+  return {
+    devtool: "source-map",
+    // contentBase: "./public",
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+      proxy: {
+        "*": {
+          target: "http://localhost:3000",
+          changeOrigin: true
+        }
       }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/, //单个时如/\.jsx$/,或者说/\.jsx?$/直接代表了.js和.jsx  ???
-        use: {
-          loader: "babel-loader"
+    },
+    resolve: {
+      // 让less-loader等插件能找到以~相对定位的资源
+      modules: [sourcedir, "node_modules"]
+    },
+    entry: {
+      main: [
+        // 编译新版本js的新api(如Promise)，主要是让IE11能够执行编译后的代码
+        "babel-polyfill",
+        //使用react-hot-loader@3.0.0-beta.6，
+        // 搭配webpack-dev-server --hot命令实现react组件的hot reload
+        isDev ? "react-hot-loader/patch" : null,
+        path.resolve(sourcedir, "index.jsx")
+      ].filter(hasValue)
+    },
+    output: {
+      path: outputdir,
+      filename: isDev ? "js/[name].js" : "js/[name]_[chunkhash:8].js",
+      // 使用require.ensure造成的延迟加载的代码文件
+      chunkFilename: isDev
+        ? "js/chunk_[id]_[name].js"
+        : "js/chunk_[name]_[chunkhash:8].js",
+      publicPath: webContextRoot
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              // 编译新版本js语法为低版本js语法
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  // 编译es2015版本的js
+                  [
+                    "babel-preset-es2015",
+                    {
+                      modules: false
+                    }
+                  ],
+                  "babel-preset-stage-0",
+                  // 编译jsx
+                  "babel-preset-react"
+                ],
+
+                plugins: [
+                  // 支持热加载react组件
+                  isDev ? "react-hot-loader/babel" : null,
+                  // 减少重复的编译后的辅助方法
+                  "babel-plugin-transform-runtime",
+                  "babel-plugin-transform-decorators-legacy",
+                  // 按需加载组件的代码和样式
+                  [
+                    "babel-plugin-import",
+                    {
+                      libraryName: "antd",
+                      style: true
+                    }
+                  ]
+                ].filter(hasValue)
+              }
+            }
+          ]
         },
-        exclude: /node_modules/
-      },
-      {
-        test: /(\.css|\.less)$/,
-        use: ["style-loader", "css-loader", "less-loader"]
-        // loader: ExtractTextPlugin.extract({
-        //   // fallback: "style-loader",
-        //   use: ["style-loader", "css-loader", "less-loader"]
-        // })
-      },
-      // {
-      //   test: /\.module\.css$/,
-      //   loader: ExtractTextPlugin.extract(
-      //     'css?sourceMap&-restructuring&modules&localIdentName=[local]___[hash:base64:5]!' +
-      //     'postcss'
-      //   ),
-      // },
-      // {
-      //   test: /\.module\.less$/,
-      //   loader: ExtractTextPlugin.extract(
-      //     'css?sourceMap&modules&localIdentName=[local]___[hash:base64:5]!!' +
-      //     'postcss!' +
-      //     `less-loader?{"sourceMap":true,"modifyVars":${JSON.stringify(theme)}}`
-      //   ),
-      // },
-      {
-        test: /.(png|svg|jpg|gif)$/,
-        use: ["url-loader?limit=2048&name=./static/images/[name].[ext]?[hash]"]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      axios: "axios",
-      "window.axios": "axios"
-    })
-  ]
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                // 第三方组件未以module方式引入css，所以不能在全局开启css module
+                modules: false
+              }
+            },
+            { loader: "postcss-loader", options: { plugins: [WebkitPrefixer] } }
+          ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: false
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: { plugins: [WebkitPrefixer] }
+            },
+            {
+              loader: "less-loader",
+              options: {
+                modules: false,
+                modifyVars: theme
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              // 编码为dataUrl的最大尺寸
+              limit: 10000,
+              // 输出路径，相对于publicPath
+              outputPath: "./static/images/",
+              name: isDev ? "[name].[ext]" : "[name]_[hash:8].[ext]"
+            }
+          }
+        },
+        {
+          test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              mimetype: "application/font-woff",
+              outputPath: "./static/fonts/",
+              name: isDev ? "[name].[ext]" : "[name]_[hash:8].[ext]"
+            }
+          }
+        },
+        {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              mimetype: "application/octet-stream",
+              outputPath: "./static/fonts/",
+              name: isDev ? "[name].[ext]" : "[name]_[hash:8].[ext]"
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        axios: "axios",
+        "window.axios": "axios"
+      }),
+      // momentjs包含大量本地化代码，需筛选
+      // 复制无需编译的文件至输出目录
+      // 自动填充js、css引用进首页
+      new HtmlWebpackPlugin({
+        title: "wzp react",
+        template: path.resolve(outputdir, "index.html"),
+        inject: "body" // Inject all scripts into the body
+      }),
+      // 设置环境变量
+      new webpack.DefinePlugin({
+        process: {
+          env: {
+            // process.env.NODE_ENV==="production"
+            // 应用代码里，可凭此判断是否运行在生产环境
+            NODE_ENV: isDev
+              ? JSON.stringify("development")
+              : JSON.stringify("production")
+          }
+        }
+      }),
+      // print more readable module names on HMR updates
+      isDev ? new webpack.NamedModulesPlugin() : null,
+      // 先清理输出目录
+      isDev ? null : new CleanWebpackPlugin([outputdir]),
+      // 排除特定库
+      isDev ? null : new webpack.IgnorePlugin(/.*/, /react-hot-loader$/),
+      // 输出报告，查看第三方库的大小
+      isDev
+        ? null
+        : new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            reportFilename: "report.html",
+            openAnalyzer: true,
+            generateStatsFile: false
+          })
+    ].filter(hasValue)
+  };
 };
